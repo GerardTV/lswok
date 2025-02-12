c4ef16a82564f2518df63d78032cafc2a25852da
gerard-inspedralbes <122524717+gerard-inspedralbes@users.noreply.github.com>
2025-02-12 12:21:57 +0100

    Initial Commit

---
+++ src/DatosWok.java
@@ -0,0 +1,9 @@
+public class DatosWok {
+    static String[] basesDesc = {"Fideos Crujientes", "Tallarines de arroz",
+            "Fideos de arroz", "Finos de arroz", "Arroz Gessami", "Arroz Integral", "Verduras Variadas", "Udon"};
+    static double[] basePrecios = {4.10, 4.10, 4.10, 4.10, 4.10, 4.10, 4.10, 5.10};
+    static boolean[] baseTamanio = {true, true, true, true, true, true, true, false};
+    static String[] salsas = {"Soja", "Ostras", "Agridulce", "Teriyaki", "Picante"};
+    static double[] preciosIngredientes = {1.50, 2.00, 1.80, 2.50, 3.00, 2.20, 1.70, 2.80};
+    static String[] ingredientesDesc = {"Pollo", "Ternera", "Gambas", "Tofu", "Huevo", "Champiñones", "Cebolla", "Pimiento"};
+}
